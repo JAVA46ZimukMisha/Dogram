133fa74da1a9612d420288125c914d231afdb615
MishaZimuk <mishazimuk@gmail.com>
2022-01-02 15:31:48 +0200

    HW07-Additional

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ console.log("Script launched");
 let detailsImage = document.querySelector(".details-image");
 let anchors = document.querySelectorAll(".thumbnails-anchor");// all HTML elements belonging to the class thumbnails-anchor
 let detailsTitle = document.querySelector(".details-title");
+let detailsNames = document.querySelectorAll(".thumbnails-title");
 for(let i = 0; i < anchors.length; i++) {
     anchors[i].addEventListener("click", function(event) {
         event.preventDefault(); //canceling default behavior of anchor element
-        setDetails(anchors[i]); //setDetails function call with passing reference to appropriate anc
-        
+        setDetails(anchors[i], detailsNames[i]); //setDetails function call with passing reference to appropriate anc 
     });
 }
-function setDetails(anchor){
+function setDetails(anchor, name){
     console.log("anchor was pressed", anchor);
     detailsImage.setAttribute("src", anchor.getAttribute("href"));
-    detailsTitle.textContent = anchor.getAttribute("data-details-title");
+    detailsTitle.textContent = name.getAttribute("data-title") + ": " + anchor.getAttribute("data-details-title");
 }
