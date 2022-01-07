abb93bd2aa433bf1d6d8f124d15ee87e56633c3b
MishaZimuk <mishazimuk@gmail.com>
2022-01-07 14:06:42 +0200

    HW-09

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ let detailsImage = document.querySelector(".details-image");
 let anchors = document.querySelectorAll(".thumbnails-anchor");// all HTML elements belonging to the class thumbnails-anchor
 let detailsTitle = document.querySelector(".details-title");
 let mainContentEl = document.querySelector(".main-content");
+let detailsSound = document.querySelector(".details-sound");
 let selectedItem;
 for(let i = 0; i < anchors.length; i++) {
     anchors[i].addEventListener("click", function(event) {
@@ -22,15 +23,25 @@ function setDetails(anchor){
     selectedItem = anchor.parentElement;
     //get element with class thumbnails-title inside the given anchor
     let thumbnailsTitleSelector = `[href="${hrefValue}"] .thumbnails-title`;
+    let thumbnailsSoundSelector = `[href="${hrefValue}"] .thumbnails-sound`;
     let thumbnailsTitleElement = document.querySelector(thumbnailsTitleSelector);
+    let thumbnailsSoundElement = document.querySelector(thumbnailsSoundSelector);
     //dog name exist inside thumbnailsTitleElement.textContent
     detailsTitle.textContent = `${thumbnailsTitleElement.textContent}: ${anchor.getAttribute("data-details-title")}` ;
-
+    let soundValue = thumbnailsSoundElement.getAttribute("src");
+    detailsSound.setAttribute("src", soundValue);
+    detailsSound.muted = false;
+    setTimeout (trueMuted, 1700);
 }
 function showDetails() {
     mainContentEl.classList.remove("hidden");
+    detailsImage.parentElement.classList.add('is-tiny');
+    setTimeout(function () {detailsImage.parentElement.classList.remove('is-tiny');},1);
 }
 function hideDetails() {
     mainContentEl.classList.add("hidden");
     selectedItem.classList.remove("selected");
+}
+function trueMuted() {
+    detailsSound.muted = true;
 }
